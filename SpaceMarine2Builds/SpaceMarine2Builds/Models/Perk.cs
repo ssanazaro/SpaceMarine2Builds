842ef2a90e26e199f1e3f9ebe9a904c229d2219e
Scott Sanazaro <s.sanazaro91@yahoo.com>
2024-11-24 09:04:43 -0600

    Added types to perks

--- SpaceMarine2Builds/SpaceMarine2Builds/Models/Perk.cs
+++ SpaceMarine2Builds/SpaceMarine2Builds/Models/Perk.cs
@@ -4,6 +4,7 @@
 	{
 		public int PerkId { get; set; }
 		public int ClassId { get; set; }
+		public string Type { get; set; }
 		public int Level { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
